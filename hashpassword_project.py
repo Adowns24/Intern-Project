e383c0620ab8254af9a73b357826735ff5f39ab2
Justin <justin@madirish.net>
2024-07-26 18:49:04 +0000

    Updated to search the database of hashes from haveibeenpwned

--- hashpassword_project.py
+++ hashpassword_project.py
@@ -28,15 +28,16 @@ def read_hashes_from_file(hashfile):
 def find_leaked_passwords(active_directory, leaked_directory):
 	#searches for leaked or reused 
 	#this should return a list of all the reused or leaked hashes 
-
+	common_hashes = set()
 	
-	#reading hashes from both directories
-	activedir_hashes = read_hashes_from_file(active_directory)#might need to change the name of this directory
+	#reading hashes from both files
+	
 	pwned_hashes= read_hashes_from_file(leaked_directory)
 	conn = sqlite3.connect("hashes.sqlite3")
 	cursor = conn.cursor()
 	# parse HaveIBeenP0wned into sqlite
 	for eachhash in pwned_hashes:
+		#print(f"Looking at: {eachhash}")
 		parsed_array = eachhash.split(":")
 		if (parsed_array[0] == "des-cbc-md5"):
 			cursor.execute('''
@@ -48,14 +49,32 @@ def find_leaked_passwords(active_directory, leaked_directory):
 			INSERT INTO userhash (hash, algo)
 			VALUES ("''' + parsed_array[0] + '''", 2)
 			''')
+	
+	# 10292304b3254f7f		
+	# Now look up values from the NTDS.dit file
+	# The text file that powers this may have multiple entries per line
+	activedir_hashes = read_hashes_from_file(active_directory)
+	
+	for user_hash in activedir_hashes:
+		#print(f"Entry is {user_hash}")
+		entries = user_hash.split(":")
+		#print("Parsing entry for " + str(entries[1]))
+		user_hash_algo = 1 if entries[1] == "des-cbc-md5" else 2
+		cursor.execute('select count(id) from userhash where algo=' + str(user_hash_algo) + ' and hash="' + entries[2] + '"')
+		row = cursor.fetchone()
+		#print("SQL output-> " + str([row[0]]))
+		if int(row[0]) > 1:
+			print("\n\r!!!Warning!!!\n\rGot a hit for " + entries[0])
+			common_hashes.add(entries[0])
+		
 	conn.commit()
 	conn.close()
    
    
 	# print(f"Active Directory Hashes: {activedir_hashes}")
 	# print(f"Pwned Directory Hashes: {pwned_hashes}")
-   #this should find the common hashes between the two directories
-	common_hashes = list(activedir_hashes.intersection(pwned_hashes))
+    #this should find the common hashes between the two directories
+	#common_hashes = list(activedir_hashes.intersection(pwned_hashes))
 	return common_hashes
 	
 def create_database(db_name):
@@ -130,7 +149,7 @@ def main():
 	#should find all the reused and leaked hashes	
 	common_hashes = find_leaked_passwords(active_directory,leaked_directory)
 
-	print(f"found {len(common_hashes)}reused or leaked hashes:")
+	print(f"found {len(common_hashes)} reused or leaked hashes:")
 	print(common_hashes)
 	
 	 #Saving common hashes to a text file
